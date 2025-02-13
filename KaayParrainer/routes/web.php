c3f6d5fd62294a28e0325508e491a28e93312b56
MariemeKmr <mariemekamara04@gmail.com>
2025-02-13 02:12:11 +0000

    Front dashboard électeurt candidat

--- KaayParrainer/routes/web.php
+++ KaayParrainer/routes/web.php
@@ -5,3 +5,34 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/CandidatDashboard', function () {
+    return view('UserDashboard.Candidats');
+});
+
+Route::get('/ElecteurDashboard', function () {
+    return view('UserDashboard.Electeurs');
+});
+
+
+// <?php
+
+// use Illuminate\Support\Facades\Route;
+// use Illuminate\Support\Facades\Auth;
+// use App\Models\candidat;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// // Dashboard Candidat avec données utilisateur
+// Route::get('/CandidatDashboard', function () {
+//     $user = Auth::user(); // Récupérer l'utilisateur connecté
+//     return view('UserDashboard.Candidats', compact('user'));
+// })->middleware('auth'); // Protéger la route
+
+// // Dashboard Électeur avec données utilisateur
+// Route::get('/ElecteurDashboard', function () {
+//     $user = Auth::user();
+//     return view('UserDashboard.Electeurs', compact('user'));
+// })->middleware('auth');
